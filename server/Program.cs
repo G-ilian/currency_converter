ccbe018c3066cf4a9d644854f914890908646484
Gabriel Ilian <66277810+G-ilian@users.noreply.github.com>
2024-09-12 13:08:35 -0300

    feat: server setup.

--- server/Program.cs
+++ server/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace server
+﻿using Grpc.Core;
+using server.services;
+using CurrencyConverter;
+
+namespace server
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Server server = null;
+            try
+            {
+                server = new Server
+                {
+                    Services = { CurrencyConverterService.BindService(new CurrencyConverterImpl()) },
+                    Ports = { new ServerPort("localhost", 5000, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine("Server listening on port 50051. Press any key to stop the server...");
+                Console.ReadKey();
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occured: " + e.Message);
+            }finally
+            {
+                if(server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
         }
     }
 }
---
+++ server/services/CurrencyConverterImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using CurrencyConverter;
+
+namespace server.services
+{
+    internal class CurrencyConverterImpl: CurrencyConverterService.CurrencyConverterServiceBase
+    {
+        public override Task<CurrencyConversionResponse> Convert(CurrencyConversionRequest request, ServerCallContext context)
+        {
+            var response = new CurrencyConversionResponse
+            {
+                From = request.From,
+                To = request.To,
+                InitialAmount = request.Amount,
+                ConvertedAmount = request.Amount * 0.85
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
