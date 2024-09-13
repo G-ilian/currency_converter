f93681d9187db9ce5932f154861429d1773b5bb4
Gabriel Ilian <66277810+G-ilian@users.noreply.github.com>
2024-09-13 12:01:28 -0300

    feat: initial configuration for requests to a external API and validate coins.

---
+++ server/enums/CurrencyCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.enums
+{
+    public enum currencyCode
+    {
+        USD, // Dólar Americano
+        EUR, // Euro
+        BRL, // Real Brasileiro
+        JPY, // Iene Japonês
+        GBP, // Libra Esterlina
+        AUD, // Dólar Australiano
+        CAD, // Dólar Canadense
+        CHF, // Franco Suíço
+        CNY, // Yuan Chinês
+        SEK, // Coroa Sueca
+        NZD, // Dólar Neozelandês
+        KRW, // Won Sul-Coreano
+        SGD, // Dólar de Singapura
+        RUB, // Rublo Russo
+        INR, // Rúpia Indiana
+    }
+}
--- server/services/CurrencyConverterImpl.cs
+++ server/services/CurrencyConverterImpl.cs
@@ -5,19 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 using Grpc.Core;
 using CurrencyConverter;
+using System.Text.Json;
+using server.enums;
 
 namespace server.services
 {
     internal class CurrencyConverterImpl: CurrencyConverterService.CurrencyConverterServiceBase
     {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        private bool isValidCurrencyCode(string code)
+        {
+            return Enum.TryParse(typeof(currencyCode), code.ToUpper(),out _);
+        }
+
         public override Task<CurrencyConversionResponse> Convert(CurrencyConversionRequest request, ServerCallContext context)
         {
+            string apiKey = "KEY_PADRAO";
+            string url = $"https://api.currencyapi.com/v3/latest";
+
+            HttpResponseMessage httpResponse;
+
+            if (!isValidCurrencyCode(request.From.Code) || !isValidCurrencyCode(request.To.Code))
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Código de moeda não suportado ou inválido"));
+            }
+
+            try
+            {
+                httpResponse = _httpClient.GetAsync($"{url}?apikey={apiKey}&base_currency={request.From.Code}&currencies={request.To.Code}").Result;
+            }
+            catch (Exception ex)
+            {
+                throw new RpcException(new Status(StatusCode.Internal, $"Falha na requisição HTTP: {ex.Message}"));
+            }
+
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                throw new RpcException(new Status(StatusCode.Internal, "Falha ao chamar a API de conversão de moedas."));
+            }
+
+            string responseContent = httpResponse.Content.ReadAsStringAsync().Result;
+
+            var responseJson = JsonSerializer.Deserialize<Dictionary<string, object>>(responseContent);
+
+            var data = responseJson["data"] as JsonElement?;
+
+            if (data == null)
+            {
+                throw new RpcException(new Status(StatusCode.Internal, "Resposta inválida da API de conversão de moedas."));
+            }
+
+            var currencyData = data?.GetProperty(request.To.Code);
+            double conversionRate = currencyData?.GetProperty("value").GetDouble() ?? 0.0;
+            double convertedAmount = request.Amount * conversionRate;
+
             var response = new CurrencyConversionResponse
             {
                 From = request.From,
                 To = request.To,
                 InitialAmount = request.Amount,
-                ConvertedAmount = request.Amount * 0.85
+                ConvertedAmount = convertedAmount
             };
 
             return Task.FromResult(response);
